56a4caec405dc1986daa25276089d6a5569e4eac
Cecília Nakamura <cecilia_nakamura@mckinsey.com>
2023-05-31 15:57:24 -0300

    basic CRUD

--- sst.config.ts
+++ sst.config.ts
@@ -1,14 +1,15 @@
-import { SSTConfig } from "sst";
-import { StorageStack } from "./stacks/StorageStack";
+import { SSTConfig } from 'sst';
+import { StorageStack } from './stacks/StorageStack';
+import { ApiStack } from './stacks/ApiStack';
 
 export default {
   config(_input) {
     return {
-      name: "notes",
-      region: "us-east-1",
+      name: 'notes',
+      region: 'us-east-1',
     };
   },
   stacks(app) {
-    app.stack(StorageStack);
+    app.stack(StorageStack).stack(ApiStack);
   },
 } satisfies SSTConfig;
