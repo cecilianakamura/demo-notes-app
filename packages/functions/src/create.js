955757d3baef056b6ef80b06d685dbc8405622aa
Cecília Nakamura <cecilia_nakamura@mckinsey.com>
2023-06-02 17:57:45 -0300

    auth

--- packages/functions/src/create.js
+++ packages/functions/src/create.js
@@ -1,7 +1,7 @@
 import { Table } from "sst/node/table";
 import * as uuid from "uuid";
-import handler from "@notes/core/handler";
-import dynamoDb from "@notes/core/dynamodb";
+import handler from "./handler"
+import dynamoDb from "./dynamodb"
 
 export const main = handler(async (event) => {
   const data = JSON.parse(event.body);
@@ -9,7 +9,7 @@ export const main = handler(async (event) => {
     TableName: Table.Notes.tableName,
     Item: {
       // The attributes of the item to be created
-      userId: "123", // The id of the author
+      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId, // The id of the author
       noteId: uuid.v1(), // A unique uuid
       content: data.content, // Parsed from request body
       attachment: data.attachment, // Parsed from request body
--- packages/functions/src/update.js
+++ packages/functions/src/update.js
@@ -1,6 +1,6 @@
 import { Table } from "sst/node/table";
-import handler from "@notes/core/handler";
-import dynamoDb from "@notes/core/dynamodb";
+import handler from "./handler"
+import dynamoDb from "./dynamodb"
 
 export const main = handler(async (event) => {
   const data = JSON.parse(event.body);
@@ -8,7 +8,7 @@ export const main = handler(async (event) => {
     TableName: Table.Notes.tableName,
     // 'Key' defines the partition key and sort key of the item to be updated
     Key: {
-      userId: "123", // The id of the author
+      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId, // The id of the author
       noteId: event.pathParameters.id, // The id of the note from the path
     },
     // 'UpdateExpression' defines the attributes to be updated
